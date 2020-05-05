3ff7e69413b8dd71d6a14ffa16f8e38d111b1294
ashwathhegde <usashwath@gmail.com>
2020-05-05 20:07:26 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+// const webpack = require('webpack');
+// const path = require('path');
+// module.exports = {
+//   entry: './src/index.js',
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: ['babel-loader'],
+        
+//       },
+//       {
+//         test: /\.s[ac]ss$/i,
+//         use: [
+//           // Creates `style` nodes from JS strings
+//           'style-loader',
+//           // Translates CSS into CommonJS
+//           'css-loader',
+//           // Compiles Sass to CSS
+//           'sass-loader',
+//         ],
+//       },
+//     ]
+    
+//   },
+//   resolve: {
+//     extensions: ['*', '.js', '.jsx']
+//   },
+//   output: {
+//     path: __dirname + '/dist',
+//     publicPath: '/',
+//     filename: 'bundle.js'
+//   },
+//   plugins: [
+//     new webpack.HotModuleReplacementPlugin()
+//   ],
+//   devServer: {
+//     disableHostCheck: true
+//   }
+// };
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      // Rules to load scss
+      {
+      // Some change here
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  }
+};
